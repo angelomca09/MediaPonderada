62217d8c85a428246d49ac0a93d6b1faeee16120
Angelo Munhoz <angelomca09@gmail.com>
2020-12-07 19:12:52 -0300

    Upload para visualização da interface

---
+++ precache-manifest.5194b588e36fe643856d7a842b4f6e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e33246ccc94b7a0efe6decf89983c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b010dd60519a63aefdb",
+    "url": "./static/js/2.15155a1f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.15155a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f620c3588e8bd32c51",
+    "url": "./static/js/main.aec5cf50.chunk.js"
+  },
+  {
+    "revision": "ec92d2367668032d839d",
+    "url": "./static/js/runtime-main.f30614b3.js"
+  }
+]);
